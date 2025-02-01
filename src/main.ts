dc3f6acf74de253e72fdea712515f87fcb438453
adelElsawaf <adelsadeq24@gmail.com>
2025-02-01 16:13:04 +0200

    fix-cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Inject ConfigService
+  const configService = app.get(ConfigService);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Strips unknown properties
@@ -12,14 +17,20 @@ async function bootstrap() {
       transform: true, // Automatically transforms payloads to DTO instances
     }),
   );
+
+  const frontEndUrl = configService.get<string>('FRONT_END_URL'); // Get FRONT_END_URL from the config
+
   app.enableCors({
-    origin: process.env.FRONT_END_URL || 'http://localhost:3000',
+    origin: frontEndUrl, // Set the origin dynamically from config
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
   });
-  console.log(app.enableCors)
+
+  console.log('CORS enabled for:', frontEndUrl);
+
   app.use(cookieParser());
+
   const port = process.env.PORT || 3001;
   await app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
